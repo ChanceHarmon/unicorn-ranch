e59901437d550caef3cc9b917a353fedc1c85651
Chance Harmon <henrikillsit@gmail.com>
2020-08-23 18:15:47 -0700

    merge to master

--- public/app.js
+++ public/app.js
@@ -1,33 +1,3 @@
 'use strict';
 console.log('connected')
 
-// const onStart = () => {
-//   for (let i = 0; i < unicornList.length; i++) {
-//     $('#barn').append(`<li id="${unicornList[i].name}">${unicornList[i].name} ${unicornList[i].color} ${unicornList[i].favoriteFood}</li>`)
-//   }
-//   populateUniList();
-//   populateLocationList();
-//   $('#move-form').on('submit', handleMove);
-// }
-
-/*
-Finish the basic concept....    DONE
-
-need to work on typing speed
-
-practice break points vs console.log
-
-
-
-
-Stretch
-Ranch to be able to use from differnt locations
-
-Database, possible server
-
-
-
-Make it look like you care
-
-
-*/
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const pg = require('pg')
 const fs = require('fs');
 const methodOverride = require('method-override');
 const path = require('path');
+const { response } = require('express');
 
 const PORT = process.env.PORT;
 
@@ -18,43 +19,56 @@ app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-
-
-
-app.get('/', (request, response) => {
-  //console.log('home route', request)
+const homeRoute = (request, response) => {
   const SQL = 'SELECT * FROM stable;';
   client.query(SQL)
     .then(result => {
-      //console.log(result.rows);
+      console.log(result)
       response.render('index', { result: result.rows });
     })
     .catch('error', error => {
       console.error(error)
     })
-})
+}
 
-app.put('/update/:id', (request, response) => {
+const updateLocation = (request, response) => {
   let { name, location, color, favorite_food } = request.body;
   let id = request.params.id;
   let SQL = `UPDATE stable SET name=$1, color=$2, favorite_food=$3, current_location=$4 WHERE id=$5;`;
   let safeValues = [name, color, favorite_food, location, id]
   client.query(SQL, safeValues)
     .then(() => {
-      // console.log(result.rows);
       response.redirect('/')
     })
     .catch('error', error => {
       console.error(error)
     })
+}
 
-})
 
-// Lets make some clean functions for these routes
+const backInTheBarn = () => {
+  let SQL = 'SELECT * FROM stable;';
+  client.query(SQL)
+    .then(data => {
+      data.rows.forEach(unicorn => {
+        console.log(unicorn, 'timeout')
+        let backInTheBarnSQL = 'UPDATE stable SET name=$1, color=$2, favorite_food=$3, current_location=$4 WHERE id=$5;';
+        let safeValues = [unicorn.name, unicorn.color, unicorn.favorite_food, 'barn', unicorn.id];
+        client.query(backInTheBarnSQL, safeValues)
+      })
+    }).catch('error', error => {
+      console.error(error)
+    })
+}
 
-//Ideally I would like the home route to query the database and populate the lists
-//I would like the update route to set the current location of the unicorn to the query location and redirect to home route, which should rerender an updated list of locations on load
 
+setInterval(() => {
+  backInTheBarn();
+}, 86400000);
+
+
+app.get('/', homeRoute);
+app.put('/update/:id', updateLocation)
 
 
 client.connect()
@@ -63,5 +77,3 @@ client.connect()
       console.log(`Unicorns are on ${PORT} clouds...`)
     })
   });
-
-  //Pretty good work, learning ajax and node without a template is different but I am getting there. Pay attention 151, that is the next hurdle
